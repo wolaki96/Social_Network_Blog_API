185ddcc889484d55aabc1cbacee78a013c762e25
Squiddy <wolaki96@gmail.com>
2022-11-29 19:26:28 -0500

    added connection

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const { connect, connection } = require('mongoose');
+
+connect('mongodb://127.0.0.1:27017/blogpost', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+});
+
+module.exports = connection;
