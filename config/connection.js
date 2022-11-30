3b100cbf0d2a71a9b87a57d62ffdb8f315b9cd21
Squiddy <wolaki96@gmail.com>
2022-11-29 19:33:50 -0500

    changed database name and added index.js to listen on port

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/blogpost', {
+connect('mongodb://127.0.0.1:27017/blogpostDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
