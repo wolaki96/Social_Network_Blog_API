eb7d5840733e92d47e7b6169ea64bf2640ea2ff4
Squiddy <wolaki96@gmail.com>
2022-11-29 19:44:49 -0500

    routes to the user

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getOneUser,
+    createUser,
+} = require('../../controllers/userController');
+//connect to user route to create a user
+router.route('/').get(getUsers).post(createUser);
+
+//connect to user route to get one user
+router.route('/:userId').get(getOneUser);
+
+
+module.exports = router;
