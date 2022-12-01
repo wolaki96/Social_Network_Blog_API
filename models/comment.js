787b29d82b4893608ce8a2c4b1ecce240aef7dcb
Squiddy <wolaki96@gmail.com>
2022-11-30 20:40:41 -0500

    added the reply schema

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const userSchema = require('./user');
 
 //schema to create a comment
@@ -30,6 +30,31 @@ const commentSchema= new Schema(
 commentSchema.virtual('replyCount').get(function (){
     return this.replies.length;
 });
+
+//creating a reaction/reply schema
+
+const replySchema = new Schema(
+    {
+    replyId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    replyBody: {
+        type: String,
+        required: true,
+        max: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: 
+    }
+    }
+)
 
 const Comment = model('comment', commentSchema);
 
