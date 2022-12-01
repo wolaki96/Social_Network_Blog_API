622d1c5dcd7044d846943d1ef15cbbd30e23e5ed
Squiddy <wolaki96@gmail.com>
2022-11-30 21:24:11 -0500

    getting a comment, deleting and updating a comment on the controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,1 +1,32 @@
 const { Comment, User } = require('../models');
+
+// get all the comments
+module.exports = {
+    getComment(req, res) {
+      Comment.find()
+        .then((comment) => res.json(comment))
+        .catch((err) => res.status(500).json(err));
+    },
+    // Getting a single comment
+    getSingleComment(req, res) {
+      Comment.findOne({ _id: req.params.commentId })
+        .select('-__v')
+        .then((comment) =>
+          !comment
+            ? res.status(404).json({ message: 'No comment with that ID has been found :(' })
+            : res.json(comment)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    //creating a comment
+    createComment(req, res) {
+        Comment.create(req.body)
+          .then((comment) => res.json(comment))
+          .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+          });
+      },
+    
+
+}
