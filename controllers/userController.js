d5de3b3d0698c7c4b8291c4ded90e98923d5155a
Squiddy <wolaki96@gmail.com>
2022-11-30 21:13:27 -0500

    user deletion

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ module.exports = {
     //get all the users and their usernames
     getUsers(req, res) {
         User.find()
-            .then((user) => res.status(200).json(users))
+            .then((users) => res.status(200).json(users))
             .catch((err) => res.status(500).json(err));
 
     },
@@ -12,7 +12,7 @@ module.exports = {
     getOneUser(req, res) {
         User.findOne({ _id: req.params.userId })
             .select('-__v')
-            .populate('replies')
+            .populate('comment')
             .populate('friends')
             .then((user) => {
                 if (!user) {
@@ -34,9 +34,14 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
 
-
-
-
+deleteUser(req, res) {
+    User.findOneAndDelete({_id: req.params.userId})
+    .then((user) =>
+    !user
+      ? res.status(404).json({ message: 'No user found' })
+      : Thought.deleteMany({ _id: { $in: user.comments } })
+  )
+  .then(() => res.status(200).json({ message: 'User deleted!' }))
+  .catch((err) => res.status(500).json(err));
 }
-
-
+}
