d41253c9e38a5e0f18e8e886a44c1e873afe1645
Squiddy <wolaki96@gmail.com>
2022-11-30 21:37:33 -0500

    added some users seeds

--- utils/user.js
+++ utils/user.js
@@ -1,3 +1,23 @@
-const user = [
-
-];
+const users = [
+    {
+        "username": "Iia Wolak",
+        "email": "itsme@gmail.com",
+        "thoughts": [],
+        "friends": [],
+        "__v": 0
+      },
+      {
+        "username": "Katie Seed",
+        "email": "friendo@friends.com",
+        "thoughts": [],
+        "friends": [],
+        "__v": 0
+      },
+      {
+      "username": "Emmett White",
+      "email": "boyfriendo@friends.com",
+      "thoughts": [],
+      "friends": [],
+      "__v": 0
+      }
+    ];
