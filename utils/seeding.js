4ddfbbba7ad6d4197f7b56b4605147c31858ba4c
Squiddy <wolaki96@gmail.com>
2022-11-30 21:45:18 -0500

    added some seeds

--- utils/seeding.js
+++ utils/seeding.js
@@ -1,3 +1,22 @@
 const connection = require('../config/connection');
+const { Comment, User } = require('../models');
 
 
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  // Dropping comments
+  await Comment.deleteMany({});
+
+  // Drop existing uesrs
+  await User.deleteMany({});
+
+  //add users
+    await User.collection.insertMany(users);
+  //add comments
+  await Comment.collection.insertMany(comments);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
