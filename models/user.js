f6c5a7f980cf28e606ac3d9100b208f30649ffbb
Squiddy <wolaki96@gmail.com>
2022-11-30 19:34:28 -0500

    worked on username and email user schema

--- models/user.js
+++ models/user.js
@@ -1,2 +1,19 @@
 const { Schema, model } = require('mongoose');
-const userSchema = require('./Assignment');
+const commentSchema = require('./comment');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        }
+    }
+)
