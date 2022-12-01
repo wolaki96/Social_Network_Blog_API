feec8f5b23e3ba86024f1c121ad288f80e291c32
Squiddy <wolaki96@gmail.com>
2022-11-30 20:06:34 -0500

    added a created at in comment js

---
+++ models/comment.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+//schema to create a comment
+const commentSchema= new Schema(
+    {
+        commentText:{
+            type: String,
+            required: true,
+            max: 280
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: timeFormat
+            }
+        }
+
+)
