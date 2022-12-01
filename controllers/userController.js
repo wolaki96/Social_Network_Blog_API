325085c1432436c7bb7a323abc4ed70a4f394517
Squiddy <wolaki96@gmail.com>
2022-11-30 21:03:41 -0500

    added create a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,35 +4,39 @@ module.exports = {
     //get all the users and their usernames
     getUsers(req, res) {
         User.find()
-        .then((user) => res.status(200).json(users))
-        .catch((err)=> res.status(500).json(err));
-        
-            },
+            .then((user) => res.status(200).json(users))
+            .catch((err) => res.status(500).json(err));
+
+    },
     //get one user
     getOneUser(req, res) {
-        User.findOne({_id: req.params.userId})
-        .select('-__v')
-        .populate('replies')
-        .populate('friends')
-        .then((user)=> {
-            if (!user) {
-                res.status(404).json({message: "No username is found with this ID."});
-                return;
-            }
-            res.json(user);
-        })
-        .catch(err => {
-            console.log(err);
-            res.sendStatus(500);
-        });
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .populate('replies')
+            .populate('friends')
+            .then((user) => {
+                if (!user) {
+                    res.status(404).json({ message: "No username is found with this ID." });
+                    return;
+                }
+                res.json(user);
+            })
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(500);
+            });
     },
 
-    
-      
-      
-      
-      
-        }
+    //create a user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
 
 
-    
+
+
+}
+
+
