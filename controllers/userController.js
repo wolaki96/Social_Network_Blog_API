014bf539f9c7118453df54a5c82d0374f3944380
Squiddy <wolaki96@gmail.com>
2022-11-30 21:18:20 -0500

    adding a friend to the friends list on controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,9 +39,29 @@ deleteUser(req, res) {
     .then((user) =>
     !user
       ? res.status(404).json({ message: 'No user found' })
-      : Thought.deleteMany({ _id: { $in: user.comments } })
+      : Comment.deleteMany({ _id: { $in: user.comments } })
   )
   .then(() => res.status(200).json({ message: 'User deleted!' }))
   .catch((err) => res.status(500).json(err));
+},
+
+// adding a friend to the user's friend list
+addFriend(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.user },
+        { $addToSet: { friends: req.body} },
+        { runValidators: true, new: true }
+      )
+        .then((user) =>
+          !user
+            ? res
+                .status(404)
+                .json({ message: 'No user found with that ID :(' })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    
 }
-}
+
+
